0c4365e1250892bd0c534ef9f7f0c9739bad3c38
Noah Nethery <nnethery@its.jnj.com>
2018-08-07 15:19:33 -0400

    initial commit

---
+++ snarf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+script="repeat
+	set minutes to random number from 1 to 60
+	set interval to minutes * 60
+	#delay interval
+	set volume 100
+	do shell script \"afplay /tmp/snarf.mp3\"
+end repeat"
+
+curl -L url -o /tmp/snarf.mp3
+
+echo "$script" | osascript
