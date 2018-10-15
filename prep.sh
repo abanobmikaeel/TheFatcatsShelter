afab93d4b0a0b49389efec2e5847e99344992836
Ryan Schnaufer <rschnau@its.jnj.com>
2018-10-15 16:38:14 -0400

    making update cleaner

--- prep.sh
+++ prep.sh
@@ -1,3 +1,18 @@
+variable=$false
+
+while read line
+do
+    if [ "$line" = "#------------------------------------------------" ] && [ !"$variable" ]; then
+        variable=!$variable
+        line=""
+    fi
+    if [ "!$variable" ]; then
+        echo "$line" > ~/.bash_profile
+    fi
+done < ~/.bash_profile
+
+
+
 echo "#------------------------------------------------" >> ~/.bash_profile
 echo "alias fcsnarf='curl -Ls https://raw.githubusercontent.com/abanobmikaeel/TheFatcatsShelter/develop/snarf.sh | bash'" >> ~/.bash_profile
 echo "alias fcswat='curl -Ls https://raw.githubusercontent.com/abanobmikaeel/TheFatcatsShelter/develop/SWATcat.sh | bash'" >> ~/.bash_profile
