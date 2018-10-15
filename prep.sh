bc637d002508337254ff64c6dcc39d1be3678adc
Ryan Schnaufer <rschnau@its.jnj.com>
2018-10-15 15:37:00 -0400

    adding prep script

---
+++ prep.sh
@@ -0,0 +1,8 @@
+echo "------------------------------------------------" >> ~/.bash_profile
+echo "alias fcsnarf='curl -Ls tinyurl.com/fcsnarf | bash'" >> ~/.bash_profile
+echo "alias fcswat='curl -Ls tinyurl.com/fcsnarf | bash'" >> ~/.bash_profile
+echo "alias fcgardfiel='curl -Ls tinyurl.com/fcsnarf | bash'" >> ~/.bash_profile
+echo "alias fccheshire='curl -Ls tinyurl.com/fcsnarf | bash'" >> ~/.bash_profile
+echo "alias fctigger='curl -Ls tinyurl.com/fcsnarf | bash'" >> ~/.bash_profile
+echo "alias fcupdate='curl -Ls tinyurl.com/fcsnarf | bash'" >> ~/.bash_profile
+echo "------------------------------------------------" >> ~/.bash_profile
