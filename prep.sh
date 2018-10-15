3f4292d01a9079839f3825e4834995e1e8365fbe
Ryan Schnaufer <rschnau@its.jnj.com>
2018-10-15 15:54:58 -0400

    updating update

--- prep.sh
+++ prep.sh
@@ -1,8 +1,10 @@
-echo "------------------------------------------------" >> ~/.bash_profile
-echo "alias fcsnarf='curl -Ls tinyurl.com/fcsnarf | bash'" >> ~/.bash_profile
+echo "#------------------------------------------------" >> ~/.bash_profile
+echo "alias fcsnarf='curl -Ls https://raw.githubusercontent.com/abanobmikaeel/TheFatcatsShelter/develop/snarf.sh | bash'" >> ~/.bash_profile
 echo "alias fcswat='curl -Ls https://raw.githubusercontent.com/abanobmikaeel/TheFatcatsShelter/develop/SWATcat.sh | bash'" >> ~/.bash_profile
 echo "alias fcgardfiel='curl -Ls https://raw.githubusercontent.com/abanobmikaeel/TheFatcatsShelter/develop/gardfiel.sh | bash'" >> ~/.bash_profile
 echo "alias fccheshire='curl -Ls https://raw.githubusercontent.com/abanobmikaeel/TheFatcatsShelter/develop/cheshire.sh | bash'" >> ~/.bash_profile
 echo "alias fctigger='curl -Ls https://raw.githubusercontent.com/abanobmikaeel/TheFatcatsShelter/develop/tigger.sh | bash'" >> ~/.bash_profile
-echo "alias fcupdate='curl -Ls https://raw.githubusercontent.com/abanobmikaeel/TheFatcatsShelter/develop/prep.sh | bash'" >> ~/.bash_profile
-echo "------------------------------------------------" >> ~/.bash_profile
+echo "alias fcupdate='curl -Ls https://raw.githubusercontent.com/abanobmikaeel/TheFatcatsShelter/fcTerminal/prep.sh | bash'" >> ~/.bash_profile
+echo "#------------------------------------------------" >> ~/.bash_profile
+echo "source ~/.bash_profile"
+echo "updated"
